e3e382283afc307941f7f313e4540cd02536839e
mariatsji <sjur.millidahl@gmail.com>
2013-02-19 20:00:00 +0100

    Elo.calculate() is now testable

--- app/models/Elo.scala
+++ app/models/Elo.scala
@@ -14,19 +14,7 @@ object Elo {
 
   val DEFAULT = 1200D
 
-  /**
-   * Returns a touple containing new Elo values for two players
-   * @param white Player white
-   * @param black Player black
-   * @param battle Battle from which elo change will be calculated
-   * @return
-   */
-  def calculate(white: Player, black: Player, battle: Battle) : (Double,Double) = {
-    if (battle.result == Outcome.UNPLAYED) throw new Exception("Cannot calculate ELO change from unplayed battle")
-
-    val whiteElo = EloDB.getCurrent(white)
-    val blackElo = EloDB.getCurrent(black)
-
+  def calculate(whiteElo: Double, blackElo: Double, outcome: Int, kFactorWhite: Long, kFactorBlack: Long) : (Double,Double) = {
     val qWhite = math.pow(10, whiteElo / 400)
     val qBlack = math.pow(10, blackElo / 400)
 
@@ -35,43 +23,30 @@ object Elo {
 
     var sWhite = 0.5
     var sBlack = 0.5
-    if (battle.result == Outcome.WHITE_WIN) {
+    if (outcome == Outcome.WHITE_WIN) {
       sWhite = 1
       sBlack = 0
-    } else if (battle.result == Outcome.BLACK_WIN) {
+    } else if (outcome == Outcome.BLACK_WIN) {
       sWhite = 0
       sBlack = 1
     }
-    val newEloWhite = whiteElo + (getKfactor(white) * (sWhite - eWhite))
-    val newEloBlack = blackElo + (getKfactor(black) * (sBlack - eBlack))
+    val newEloWhite = whiteElo + (kFactorWhite * (sWhite - eWhite))
+    val newEloBlack = blackElo + (kFactorBlack * (sBlack - eBlack))
 
     (newEloWhite,newEloBlack)
   }
 
-  /**
-   * FIDE rules:
-   * K = 30 (was 25) for a player new to the rating list until s/he has completed events with a total of at least 30 games.[15]
-   * K = 15 as long as a player's rating remains under 2400.
-   * K = 10 once a player's published rating has reached 2400, and s/he has also completed events with a total of at least 30 games. Thereafter it remains permanently at 10.
-   */
-  def getKfactor(player: Player): Float = {
-    def playedMatches: Int = BattleDB.allEverMatchesForPlayer(player.id).size
-    if (playedMatches < 30) 30
-    else {
-        if(EloDB.getCurrent(player) < 2400) 15 else 10
-    }
-  }
 }
 
 object EloDB {
   /**
-   * The rowparser
+    * The rowparser
    */
   val elo = {
     get[Long]("id") ~
-      get[Long]("player") ~
-      get[Long]("battle") ~
-      get[Double]("elo") map {
+    get[Long]("player") ~
+    get[Long]("battle") ~
+    get[Double]("elo") map {
       case id ~ player ~ battle ~ elo => Elo(id, player, battle, elo)
     }
   }
@@ -92,19 +67,26 @@ object EloDB {
     DB.withConnection {
       implicit c =>
         SQL("INSERT INTO elo (player, elo) VALUES ({player},{elo})").on(
-          "player" -> player.id, "elo" -> elo).executeInsert()
+        "player" -> player.id, "elo" -> elo).executeInsert()
     } match {
       case Some(id: Long) => getById(id)
       case None => throw new Exception(
-        "SQL Error - Did not save Battle"
-      )
+      "SQL Error - Did not save Battle"
+    )
     }
   }
 
-  def getCurrent(player: Player): Double =
-    if (allForPlayer(player.id).isEmpty)
-      Elo.DEFAULT
-    else
-      allForPlayer(player.id).last.elo
-
+  /**
+   * FIDE rules:
+   * K = 30 (was 25) for a player new to the rating list until s/he has completed events with a total of at least 30 games.[15]
+   * K = 15 as long as a player's rating remains under 2400.
+   * K = 10 once a player's published rating has reached 2400, and s/he has also completed events with a total of at least 30 games. Thereafter it remains permanently at 10.
+   */
+  def getKfactor(player: Player): Float = {
+    def playedMatches: Int = BattleDB.allEverMatchesForPlayer(player.id).size
+    if (playedMatches < 30) 30
+    else {
+      if(player.currentElo < 2400) 15 else 10
+    }
+  }
 }
--- test/models/EloSpec.scala
+++ test/models/EloSpec.scala
@@ -1,83 +1,32 @@
 package models
 
 import org.specs2.mutable._
-import play.api.test.FakeApplication
-import play.api.test.Helpers._
 
 class EloSpec extends Specification {
 
   "A win for player 1" should {
     "increase ELO for player 1" in {
-      running(FakeApplication()) {
-        PlayerDB.create("1")
-        PlayerDB.create("2")
-        TournamentDB.create("test", new java.util.Date())
-        val whiteWin = new Battle(1, 1, 2, Outcome.WHITE_WIN, 1)
-        Elo.calculate(PlayerDB.getById(1), PlayerDB.getById(2), whiteWin)._1 must beGreaterThan(Elo.DEFAULT)
-      }
+      Elo.calculate(1200, 1200, Outcome.WHITE_WIN, 30, 30)._1 must beGreaterThan(Elo.DEFAULT)
     }
     "decrease ELO for player 2" in {
-      running(FakeApplication()) {
-        PlayerDB.create("1")
-        PlayerDB.create("2")
-        TournamentDB.create("test", new java.util.Date())
-        val whiteWin = new Battle(1, 1, 2, Outcome.WHITE_WIN, 1)
-        Elo.calculate(PlayerDB.getById(1), PlayerDB.getById(2), whiteWin)._2 must beLessThan(Elo.DEFAULT)
-      }
+      Elo.calculate(1200, 1200, Outcome.WHITE_WIN, 30, 30)._2 must beLessThan(Elo.DEFAULT)
     }
   }
 
   "A draw for two equally rated players" should {
     "not change ELO for player 1" in {
-      running(FakeApplication()) {
-        PlayerDB.create("1")
-        PlayerDB.create("2")
-        TournamentDB.create("test", new java.util.Date())
-        val whiteWin = new Battle(1, 1, 2, Outcome.DRAW, 1)
-        Elo.calculate(PlayerDB.getById(1), PlayerDB.getById(2), whiteWin)._1 must beEqualTo(Elo.DEFAULT)
-      }
+      Elo.calculate(1200, 1200, Outcome.DRAW, 30, 30)._1 must beEqualTo(Elo.DEFAULT)
     }
     "not change ELO for player 2" in {
-      running(FakeApplication()) {
-        PlayerDB.create("1")
-        PlayerDB.create("2")
-        TournamentDB.create("test", new java.util.Date())
-        val whiteWin = new Battle(1, 1, 2, Outcome.DRAW, 1)
-        Elo.calculate(PlayerDB.getById(1), PlayerDB.getById(2), whiteWin)._2 must beEqualTo(Elo.DEFAULT)
-      }
+      Elo.calculate(1200, 1200, Outcome.DRAW, 30, 30)._2 must beEqualTo(Elo.DEFAULT)
     }
   }
 
   "Two wins against unequally rated players" should {
     "give more Elo for win against strong player" in {
-      running(FakeApplication()) {
-        //player
-        PlayerDB.create("Player Under Test")
-        //strong
-        PlayerDB.create("Strong player")
-        //weak
-        PlayerDB.create("Weak player")
-        TournamentDB.create("test", new java.util.Date())
-        //make palyer 2 strong and 3 weak
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        BattleDB.create(new Pairing(PlayerDB.getById(2), PlayerDB.getById(3)), TournamentDB.getById(1))
-        1 to 8 foreach(BattleDB.setResult(_, Outcome.WHITE_WIN))
-
-        //win against weak
-        val smallBattle = new Battle(1, 1, 3, Outcome.WHITE_WIN, 1)
-        val smallWin = Elo.calculate(PlayerDB.getById(1), PlayerDB.getById(3), smallBattle)._1
-        //win against strong
-        val bigBattle = new Battle(1, 1, 2, Outcome.WHITE_WIN, 1)
-        val bigWin = Elo.calculate(PlayerDB.getById(1), PlayerDB.getById(2), bigBattle)._1
-
-        bigWin must beGreaterThan(smallWin)
-      }
+      val smallWin = Elo.calculate(1200, 1000, Outcome.WHITE_WIN, 30, 30)._1
+      val bigWin = Elo.calculate(1200, 1600, Outcome.WHITE_WIN, 30, 30)._1
+      bigWin must beGreaterThan(smallWin)
     }
   }
 
