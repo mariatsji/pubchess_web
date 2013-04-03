48ab0742059bb66e36aceddfa8848f1142936778
mariatsji <sjur.millidahl@gmail.com>
2013-04-03 21:19:20 +0200

    save match i gang

--- app/controllers/ChessApplication.scala
+++ app/controllers/ChessApplication.scala
@@ -33,8 +33,8 @@ object ChessApplication extends Controller {
       case "black" => Outcome.BLACK_WIN
       case "draw" => Outcome.DRAW
     }
-    battle.finish(outcome)
-    Ok(myTuple._1 + " , " + myTuple._2 + " , " + myTuple._3)
+    battle.finish(outcome, myTuple._2.toInt, myTuple._1.toInt)
+    Ok("Saved " + outcome + " with # beers white : " + myTuple._2 + " and # beers black : " + myTuple._1)
   }
 
   val playerForm = Form("name" -> nonEmptyText)
